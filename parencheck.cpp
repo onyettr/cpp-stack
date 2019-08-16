73bdf5422971b51dd56e076033a754079e35a825
Richard Onyett <ronyett@megachips.com>
2019-08-16 15:57:11 -0700

    fixed issue with match function outputs which paren is in error

--- parencheck.cpp
+++ parencheck.cpp
@@ -58,25 +58,36 @@ Prototypes of all functions contained in this file (in order of occurance)
 */
 Stack<char> MyStack(10);
 
-int ParenthesisMatch(char *CheckString) {
+bool ParenthesisMatch(char *CheckString) {
   char *pString = CheckString;
   int Check = 1;
+  int charpos = 0;
   
   while ( *pString != 0 ) {
     if (*pString == '(') {
       MyStack.push(*pString);
-    } else if (!MyStack.isEmpty() && (*pString == ')') && MyStack.peek() == ')') {
+    } else if (!MyStack.isEmpty() && ((*pString == ')') && MyStack.peek() == '(')) {
       MyStack.pop();
     } else {
       Check = 0;
+      break;
     }
     pString++;
+    charpos++;
   }
 
   if (Check && MyStack.isEmpty()) {
-    return 0;
+    return true;
   } else {
-    return 1;
+    //    cout << charpos << endl;
+    cout << endl;
+    cout << CheckString << endl;
+    for (int i=0; i < charpos-1; i++) {
+      cout << " ";
+    }
+    cout << "^" << endl;
+    
+    return false;
   }
 }
 
@@ -84,25 +95,28 @@ int main ( void ) {
   cout << "Parenthesis Checker" << endl;
 
   char Exp1[] = "()";
-  cout << Exp1 << " " << (ParenthesisMatch(Exp1) == 1 ? "Balanced" : "Not Balanced") << endl;   
+  cout << Exp1 << " " << ParenthesisMatch(Exp1) << endl;   
   
   char Exp2[] = "(())";
-  cout << Exp2 << " " << (ParenthesisMatch(Exp2) == 1 ? "Balanced" : "Not Balanced") << endl;   
+  cout << Exp2 << " " << ParenthesisMatch(Exp2) << endl;   
 
-  char Exp5[] = "(()";
-  cout << Exp5 << " " << (ParenthesisMatch(Exp5) == 1 ? "Balanced" : "Not Balanced") << endl;   
-  
   char Exp3[] = "(((((())))))";
-  cout << Exp3 << " " << (ParenthesisMatch(Exp3) == 1 ? "Balanced" : "Not Balanced") << endl;   
+  cout << Exp3 << " " << ParenthesisMatch(Exp3) << endl;   
 
   char Exp4[] = "(";
-  cout << Exp4 << " " << (ParenthesisMatch(Exp4) == 1 ? "Balanced" : "Not Balanced") << endl;   
+  cout << Exp4 << " " << ParenthesisMatch(Exp4) << endl;   
+
+  char Exp5[] = "(()";
+  cout << Exp5 << " " << ParenthesisMatch(Exp5) << endl;   
 
   char Exp6[] = "(()))";
-  cout << Exp6 << " " << (ParenthesisMatch(Exp6) == 1 ? "Balanced" : "Not Balanced") << endl;   
+  cout << Exp6 << " " << ParenthesisMatch(Exp6)  << endl;   
 
-  char Exp7[] = "(((((((((()))";
-  cout << Exp7 << " " << (ParenthesisMatch(Exp7) == 1 ? "Balanced" : "Not Balanced") << endl;   
+  char Exp7[] = "(((((((()))";
+  cout << Exp7 << " " << ParenthesisMatch(Exp7)  << endl;   
+
+  char Exp8[] = "())))))))";
+  cout << Exp8 << " " << ParenthesisMatch(Exp8)  << endl;   
   
   exit(0);
 }
