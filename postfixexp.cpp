65743d0a56e78a8bd3a9a14012ddfa001b243a28
ronyett <ronyett@megachips.com>
2019-08-19 16:29:11 -0700

    using string through out

--- postfixexp.cpp
+++ postfixexp.cpp
@@ -113,37 +113,35 @@ int OperandExecute(int value1, int value2, char operand) {
 
  int main ( void ) {
    cout << "Postfix Expression" << endl;
-   char commandline[20];
-   char ParsedBuffer[10];
-   int j = 0;
+   string commandline;
   
    cout << "> ";
-   fgets(commandline, 20, stdin);
-   //   gets(commandline);   
+   //   fgets(commandline, 20, stdin);
+   getline(cin, commandline);   
 
-   for (int i=0; i < (int)strlen(commandline); i++) {
-    if (commandline[i] >= '0' && commandline[i] <= '9') {    /* Its a digit  */
-      ParsedBuffer[j++] = commandline[i];      
-    } else if (commandline[i] == ' ') {                      /* End of a digit(s) */
-      if ( j > 0 ) {
-	ParsedBuffer[j] = '\0';
-	PostFixStack.push(atoi(ParsedBuffer));
-	j = 0;
-      }
-    } else if (isOperator(commandline[i])) {                 /* Operand seen */
-      int value1 = 0;
-      int value2 = 0;
-      int result = 0;
-      char operand;
-      
-      value1 = PostFixStack.pop();
-      value2 = PostFixStack.pop();
-      operand= commandline[i];
-      
-      result = OperandExecute(value1, value2, operand);
-      PostFixStack.push(result);
-    }
-  }
+   for (int i=0; i < (int)commandline.length(); i++) {
+     if (isdigit(commandline[i])) {                          /* Its a digit(s)  */
+       int intvalue = 0;
+       while (i < (int)commandline.length() && isdigit(commandline[i])){
+	 intvalue = (intvalue * 10) + (commandline[i] -'0');
+	 i++;
+       }
+
+       PostFixStack.push(intvalue);       
+     } else if (isOperator(commandline[i])) {                 /* Operand seen */
+       int value1 = 0;
+       int value2 = 0;
+       int result = 0;
+       char operand;
+
+       value1 = PostFixStack.pop();
+       value2 = PostFixStack.pop();
+       operand= commandline[i];
+
+       result = OperandExecute(value1, value2, operand);
+       PostFixStack.push(result);
+     }
+   }
 
    cout << "Result = " << PostFixStack.peek() << endl;
 
